93aea7e0cff5ff790b1c751d0ba9223a108c2565
jithin <jithinsyamms@gmail.com>
2022-01-29 14:43:22 +0530

    Adding Arrays

---
+++ Arrays/src/MaxSubArray.java
@@ -0,0 +1,34 @@
+
+public class MaxSubArray {
+
+	public static void main(String args[]) {
+		MaxSubArray maxSubArray = new MaxSubArray();
+		int arr[] = {-2,-3,-4,-1,-5};
+		maxSubArray.maxSubArray(arr, arr.length);
+	}
+	
+	private void maxSubArray(int arr[], int n) {
+		
+		int curSum = 0;
+		int bestSum = Integer.MIN_VALUE;
+		int start = 0,end = 0,s = 0;
+		
+		for (int i = 0 ; i < n ; i ++ ) {
+			curSum += arr[i];
+			if (bestSum < curSum) {
+				bestSum = curSum;
+				start = s;
+				end = i;
+			}
+			if(curSum < 0) {
+				curSum = 0;
+				s = i+1;
+			}
+		}
+		
+		System.out.println("Max sum is " + bestSum + " between " + start + " and " + end);
+		
+		
+	}
+	
+}
